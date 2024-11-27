71f5904f47bdda6bd4da8ea55911c306857f15dd
Swarnab <swarnabmessibanerjee@gmail.com>
2024-11-27 20:22:03 +0530

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        className="min-h-screen bg-gradient-to-br
+    from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden"
+      ></div>
+    </>
+  );
+}
+
+export default App;
