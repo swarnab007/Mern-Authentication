62f2eafe0fb934a420456ee04bec830fcf02003c
Swarnab <swarnabmessibanerjee@gmail.com>
2024-11-29 08:39:06 +0530

    Frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,7 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import FloatingShape from "./components/FloatingIcon";
+import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
@@ -6,7 +9,35 @@ function App() {
       <div
         className="min-h-screen bg-gradient-to-br
     from-gray-900 via-green-900 to-emerald-900 flex items-center justify-center relative overflow-hidden"
-      ></div>
+      >
+        <FloatingShape
+          color="bg-green-500"
+          size="w-64 h-64"
+          top="-5%"
+          left="10%"
+          delay={0}
+        />
+        <FloatingShape
+          color="bg-emerald-500"
+          size="w-48 h-48"
+          top="70%"
+          left="80%"
+          delay={5}
+        />
+        <FloatingShape
+          color="bg-lime-500"
+          size="w-32 h-32"
+          top="40%"
+          left="-10%"
+          delay={2}
+        />
+
+        <Routes>
+          {/* Add routes here */}
+          <Route path="/" element={"Home"} />
+          <Route path="/register" element={<RegisterPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
